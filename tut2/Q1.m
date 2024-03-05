52e78a407a4f4c0fc961258b54e2b03e7059f39a
nikhil061102 <106477217+nikhil061102@users.noreply.github.com>
2024-03-05 15:29:34 +0530

    done q1

--- tut2/Q1.m
+++ tut2/Q1.m
@@ -1,15 +1,36 @@
-m = input('Enter the number of variables: ');
-n = input('Enter the number of constraints: ');
+clc
+clear all 
+format short 
+
+n = input('Enter the number of variables: ');
+m = input('Enter the number of constraints: ');
 A = [];
 b = [];
-for i=1:n
-    equalConstraint = input(['Coefficients for constraints ', num2str(i), ': ']);
-    A = [A; equalConstraint]; % coeff matrix
-    rhsConstraint = input(['RHS for constraint ', num2str(i), ': ']);
-    b = [b; rhsConstraint]; % constraint rhs values
-end  
+noOfLesserThanEq=input('The number of the less than constraints: ');
+noOfequalConstraint=input('The number of the equality constraints: ');
+n = n+noOfLesserThanEq;
+if(noOfLesserThanEq > 0)
+    fprintf("Enter constraints for lesser than constraint below :- \n");
+    for i=1:noOfLesserThanEq
+        lessConstraint = input(['Coefficients for constraints ', num2str(i), ': ']);
+        A = [A; lessConstraint]; % coeff matrix
+        rhsConstraint = input(['RHS for constraint ', num2str(i), ': ']);
+        b = [b; rhsConstraint]; % constraint rhs values
+    end  
+    A = [A eye(noOfLesserThanEq)];
+end
+if(noOfequalConstraint > 0)
+    fprintf("Enter constraints for equality constraint below :- \n");
+    for i=1:noOfequalConstraint
+        equalConstraint = input(['Coefficients for constraints ', num2str(i), ': ']);
+        equalConstraint = [equalConstraint zeros(1,noOfLesserThanEq)];
+        A = [A; equalConstraint]; % coeff matrix
+        rhsConstraint = input(['RHS for constraint ', num2str(i), ': ']);
+        b = [b; rhsConstraint]; % constraint rhs values
+    end
+end
 c = input('Coefficients for linear objective function : ');
-
+c = [c zeros(1,noOfLesserThanEq)];
 f = @(X) c*X;
 C = nchoosek(n,m); % calc nCm
 D = nchoosek(1:n,m); % compute all combinations
@@ -23,22 +44,8 @@ for i=1:C
     for j=1:m
         B = [B A(:,index(j))];
     end
-    Y = inv(B)*b;
-    X(index) = Y;
-    X
-    if(X >= 0)
-        fs = [fs X];
-        z = [z f(X)];
-    else 
-        ifs = [ifs X];
-    end
-end
-for i=1:C
-    X= zeros(n,1); % initial solution matrix
-    index = D(i,:); 
-    B = [];
-    for j=1:m
-        B = [B A(:,index(j))];
+    if(det(B) == 0)
+        continue
     end
     Y = inv(B)*b;
     X(index) = Y;
@@ -49,6 +56,7 @@ for i=1:C
         ifs = [ifs X];
     end
 end
+    
 fprintf("So all feasible soln and their respective z values are :-\n");
 for i=1:length(z)
     fprintf("%d) ",i)
